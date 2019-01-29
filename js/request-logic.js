618f2eca28a03b0125cdb3e83247a10b3046b2b0
Daniel Pereira <danielpaulpereira@protonmail.com>
2019-01-29 06:47:03 -0500

    All tests pass.

--- js/request-logic.js
+++ js/request-logic.js
@@ -5,4 +5,19 @@
 
 function getContentType(filename) {
   const extension = filename.match(/.*\.([^\.]*)$/)[1];
+
+  switch (extension) {
+    case 'html':
+      return "text/html";
+      break;
+    case 'css':
+      return "text/css";
+    case 'jpeg':
+    case 'jpg':
+      return "image/jpeg";
+      break;
+    default:
+      return 'text/plain';
+      break;
+  }
 }
