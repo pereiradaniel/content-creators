bf79ebe3e34d873b3df6c711beea4b4f21bda1e2
Daniel Pereira <danielpaulpereira@protonmail.com>
2019-01-29 06:48:52 -0500

    Add comment.

--- js/request-logic.js
+++ js/request-logic.js
@@ -1,11 +1,7 @@
-// We at Content Creators know this code is useful for getting the
-// extension off of the supplied filename, but we can't figure out the rest of
-// the function to use it! We hope this is useful to you!
-
-
 function getContentType(filename) {
   const extension = filename.match(/.*\.([^\.]*)$/)[1];
 
+// Switch appropriately responds to the file extension.
   switch (extension) {
     case 'html':
       return "text/html";
